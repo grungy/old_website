a980963f100851150a01de0572dbdea51ee0c6b7
marks <marksua@gmail.com>
2020-08-01 18:27:10 -0400

    recreated wordpress site and added favorite posts.

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,37 @@
-var store = []
+var store = [{
+        "title": "16 Bit DAC FMC Card",
+        "excerpt":"We required a higher resolution and faster Digital-toAnalog Converter (DAC) for our IRLED micro-display screen. The DAC in this program is responsible for setting the brightness of the IRLED pixel. A little overview diagram of the IRLED micro-display system would help understand the architecture. Below is a block diagram of...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/portfolio/16-bit-dac-fmc/",
+        "teaser": "http://localhost:4000/assets/images/16-bit-dac/dac_image.png"
+      },{
+        "title": "CMOS IC Yield Analysis",
+        "excerpt":"Overview I had the opportunity to be the Principal Investigator on a phase 2 SBIR Air Force program called AERIA. In this program the goal was to make rectangular shaped Infrared LED (IRLED) emitter hybrids. The hybrids consisted of a CMOS die called a Read-In-Integrated-Circuit (RIIC), and an IRLED emitter...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/portfolio/cmos-ic-yield-analysis/",
+        "teaser": "http://localhost:4000/assets/images/cmos-ic-yield-analysis/clean_room_setup.jpg"
+      },{
+        "title": "current op-amp testing",
+        "excerpt":"Summary: I replaced the feedback resistor elements with 510 ohm resistors for Rf and Rg. This gives a gain of two and the widest bandwidth, 1.4GHz, from the OPA695. Opamps behave much better. No more 416MHz ringing. I also removed the feedback capacitor because I read that this causes instability...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/portfolio/current-op-amp-testing/",
+        "teaser": "http://localhost:4000/assets/images/op-amp-testing/21_1_board.png"
+      },{
+        "title": "Static Electromagnetic Simulator",
+        "excerpt":"static-bs Is a simple 3D electro-magnetostatic biot-savart solving simulator written in python. I wrote this simulator for some research involving detecting the direction of flow of sea water with a static magnetic field. Charged particles in the water traveled through the magnetic field and caused a corresponding voltage gradient. Plates...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/portfolio/staticbs/",
+        "teaser": "http://localhost:4000/assets/images/staticbs/regular_octagon_helmholtz_71.53mm_radius.png"
+      },{
+        "title": "Favorite Textbooks",
+        "excerpt":"The Design of CMOS Radio-Frequency Integrated Circuits by Thomas H. Lee High Speed Digital Design: A Handbook of Black Magic by Howard Johnson and Martin Graham The Art of Electronics: The x Chapters by Paul Horowitz and Winfield Hill The Art of Electronics by Paul Horowitz and Winfield Hill Switching...","categories": ["posts"],
+        "tags": ["books","resources"],
+        "url": "http://localhost:4000/posts/favorite-textbooks/",
+        "teaser": null
+      },{
+        "title": "Favorite Youtube Resources",
+        "excerpt":"Electronics      EEVblog   Applied Science   w2aew   Fabricobbling / Manufacturing      AvE   Math / Physics      Minute Physics   3blue1brown   Numberphile   Control Theory      Steve Brunton   A.I. / Machine Learning      Lex Fridman  ","categories": ["posts"],
+        "tags": ["books","resources"],
+        "url": "http://localhost:4000/posts/favorite-youtube/",
+        "teaser": null
+      },]
